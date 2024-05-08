c930a7986d2b17e9692ed7b96475881ffc2d5566
Thiago Freitas <66418848+T466@users.noreply.github.com>
2024-05-07 22:00:28 -0300

    Update check_waagent-deb.sh

--- check_waagent-deb.sh
+++ check_waagent-deb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-systemctl status waagent.service | grep Active | awk '{print $2}' > /tmp/statuswaagent.log
+systemctl status walinuxagent | grep Active | awk '{print $2}' > /tmp/statuswaagent.log
 waagent --version | grep Goal | awk '{print $4}' > /tmp/versionwaagent.log
 status=$(cat /tmp/statuswaagent.log)
 
@@ -8,8 +8,7 @@ if [ $status = active ]; then
     echo "Rodando"
 else
     echo "Parado, iniciando ele"
-systemctl enable waagent.service
+systemctl enable walinuxagent
 sleep 1
-systemctl start waagent.service
-
+systemctl start walinuxagent
 fi
